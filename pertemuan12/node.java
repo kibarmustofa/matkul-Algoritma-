568851c5278395fdfef2cd254cee27e3d27d00db
kibarmustofa <kibarmustofa115@gmail.com>
2024-05-13 12:16:50 +0700

    percobaan1

---
+++ pertemuan12/node.java
@@ -0,0 +1,12 @@
+package pertemuan12;
+
+public class node {
+    int data;
+    node prev, next;
+
+    node(node prev, int data, node next ){
+        this.prev=prev;
+        this.data=data;
+        this.next=next;
+    }
+}
